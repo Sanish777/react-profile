d8037bcbf561161d475d9c4f807f3bc84ab8a113
Sanish Shakya <sanishsoftnep@gmail.com>
2022-01-26 12:25:58 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import About from './components/About';
-import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
-import Portfolio from './components/Portfolio';
-import Quote from './components/Quote';
 import Skills from './components/Skills';
 import Slider from './components/Slider';
 
