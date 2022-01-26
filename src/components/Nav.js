52e3b490bd10e668741fbe4046bcb579395035e1
Sanish Shakya <sanishsoftnep@gmail.com>
2022-01-26 12:56:45 +0545

    changes1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
         <div className="row">
           <div className="col-12">
             <nav className="main-nav">
-              <a href="index.html" className="logo">  
+              <a href="/#" className="logo">  
                 <img src="assets/images/logo-v3.png" alt="Logo" />
               </a>
               <ul className="nav">
